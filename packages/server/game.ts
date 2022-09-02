47fb56919003b07fb0344dbd7158b2cef57d3646
bananu7 <bananu7@o2.pl>
2022-09-02 10:47:46 +0200

    WIP on setting the basic match structure

---
+++ packages/server/game.ts
@@ -0,0 +1,88 @@
+
+export type Action = 'Move' | 'Attack' | 'Harvest'
+
+export type Player = number
+
+export type UnitKind = 'Harvester' | 'Marine' | 'Tank'
+export type Unit = {
+    actionQueue: Action[]
+    kind: UnitKind,
+    owner: Player,
+}
+
+export type BuildingKind = 'Base' | 'Barracks'
+export type Building = {
+    kind: BuildingKind,
+    owner: Player,
+}
+
+export type Game = {
+    state: GameState
+    players: Player[],
+    board: Board,
+}
+
+export type GameMap = {
+    tiles: number[],
+    w: number,
+    h: number,
+};
+
+export type Board = {
+    map: GameMap,
+    units: Unit[],
+    buildings: Building[],
+}
+
+export type GameState = {
+    id: 'Fresh',
+} | {
+    id: 'Precount', // once everyone joins
+    count: number,
+} | {
+    id: 'Play',
+} | {
+    id: 'Paused',
+} | {
+    id: 'GameEnded',
+}
+
+export function newGame(map: GameMap): Game {
+    return {
+        state: {id: 'Fresh'},
+        players: [],
+        board: {
+            map: map,
+            units: [],
+            buildings: [],
+        }
+    }
+}
+
+export function tick(dt: number, g: Game): Game {
+    switch (g.state.id) {
+    case 'Precount':
+        g.state.count -= dt;
+        if (g.state.count <= 0) {
+            g.state = {id: 'Play'};
+        }
+        break;
+    case 'Play':
+        const e = eliminated(g);
+        // TODO alliances, actual game type etc.
+        if (e.length > 0) {
+            g.state = {id: 'GameEnded'};
+        }
+    }
+
+    return g;   
+}
+
+function eliminated(g: Game): Player[] {
+    const buildingsByPlayer = (p: Player) => g.board.buildings.filter(b => b.owner === p);
+
+    const buildingCounts = g.players.map(p => [p, buildingsByPlayer(p).length]);
+
+    return buildingCounts.filter(([p,c]) => c === 0).map(([p,c]) => p);
+}
+
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,13 +1,44 @@
 import geckos, { Data } from '@geckos.io/server'
+import http from 'http'
+import express from 'express'
 
+import {Game, newGame, tick} from './game';
+
+const app = express()
+const server = http.createServer(app)
 const io = geckos()
 
-io.listen(9208) // default port is 9208
+const games : Game[] = [];
 
+type IdentificationPacket = {
+  player: number,
+  gameId: string, 
+}
+
+io.addServer(server)
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.post('/create', (req, res) => {
+    // TODO - load or w/e
+    const map = { w: 10, h: 10, tiles: [1,2,3] };
+
+    games.push(newGame(map));
+})
+
+// channel is a new RTC connection (i.e. one browser)
 io.onConnection(channel => {
+  // first figure out where to join it
   channel.onDisconnect(() => {
     console.log(`${channel.id} got disconnected`)
   })
+
+  channel.on('join', (data: Data) => {
+    const p = data as IdentificationPacket;
+    channel.join(p.gameId)
+  });
 
   channel.on('chat message', (data: Data) => {
     console.log(`got ${data} from "chat message"`)
@@ -15,3 +46,5 @@ io.onConnection(channel => {
     io.room(channel.roomId).emit('chat message', data)
   })
 })
+
+server.listen(9208)
