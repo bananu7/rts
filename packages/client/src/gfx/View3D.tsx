2e6f3550115fe8dad182fa34ebb9b635f0a525dd
bananu7 <bananu7@o2.pl>
2022-09-10 10:50:14 +0200

    Added fps display

--- packages/client/src/gfx/View3D.tsx
+++ packages/client/src/gfx/View3D.tsx
@@ -1,9 +1,25 @@
 import { ReactThreeFiber, Canvas, extend, useThree, useFrame } from '@react-three/fiber'
 import { Suspense, useRef, useEffect, useLayoutEffect, useState, CSSProperties } from 'react'
 
+import StatsImpl from 'three/examples/jsm/libs/stats.module.js'
+
 import * as THREE from 'three';
 
 import { MapControls } from './MapControls'
+
+function Stats() {
+  const [stats] = useState(() => new StatsImpl())
+  useEffect(() => {
+    stats.showPanel(0)
+    document.body.appendChild(stats.dom)
+    return () => document.body.removeChild(stats.dom)
+  }, [stats])
+  return useFrame(state => {
+    stats.begin()
+    state.gl.render(state.scene, state.camera)
+    stats.end()
+  }, 1)
+}
 
 function CameraControls() {
     const { camera, gl: { domElement }, scene } = useThree();
@@ -138,6 +154,7 @@ export function View3D(props: Props) {
                     <ambientLight />
                     <MapSpotlight />
                     {props.children}
+                    <Stats />
                 </Canvas>
             </div>
         </Suspense>
