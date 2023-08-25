8a0d5ed2faa26f77345368725b753fd95cd7dc06
bananu7 <bananu7@o2.pl>
2023-08-25 23:32:43 +0200

    Added the logic to the trivial case of unit placement after produce

--- packages/server/src/game/produce.ts
+++ packages/server/src/game/produce.ts
@@ -4,6 +4,7 @@ import { getUnitReferencePosition } from './util.js'
 export function findPositionForProducedUnit(unit: Unit, type: string): Position {
 
     const refPos = getUnitReferencePosition(unit);
+    refPos.y += 4;
 
     // TODO implement
     return refPos;
