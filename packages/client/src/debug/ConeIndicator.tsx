5555b5bee99009abb5c0969d20b6b88147322d18
bananu7 <bananu7@o2.pl>
2024-06-26 01:04:33 +0200

    Moved ConeIndicator to a separate file

---
+++ packages/client/src/debug/ConeIndicator.tsx
@@ -0,0 +1,27 @@
+import { ThreeCache } from '../gfx/ThreeCache'
+import * as THREE from 'three';
+const cache = new ThreeCache();
+
+const coneGeometry = new THREE.ConeGeometry(0.5, 2, 8);
+export function ConeIndicator(props: {action: UnitAction, smoothing: boolean}) {
+    // TODO - this will be replaced with animations etc
+    let indicatorColor = 0xeeeeee;
+    if (props.action === 'Moving')
+        indicatorColor = 0x55ff55;
+    else if (props.action === 'Attacking')
+        indicatorColor = 0xff5555;
+    else if (props.action === 'Harvesting')
+        indicatorColor = 0x5555ff;
+    // indicate discrepancy between server and us
+    else if (props.smoothing)
+        indicatorColor = 0xffff55;
+
+    return (
+        <mesh
+            position={[0, 5, 0]}
+            rotation={[0, 0, -1.57]}
+            geometry={coneGeometry}
+            material={cache.getBasicMaterial(indicatorColor)}
+        />
+    );
+}
--- packages/client/src/gfx/Unit3D.tsx
+++ packages/client/src/gfx/Unit3D.tsx
@@ -17,6 +17,7 @@ import { ThreeCache } from './ThreeCache'
 import { FileModel } from './FileModel'
 import { UnitDisplayEntry } from './UnitDisplayCatalog'
 import { Horizon } from '../debug/Horizon'
+import { ConeIndicator } from '../debug/ConeIndicator'
 import { debugFlags } from '../debug/flags'
 
 const cache = new ThreeCache();
@@ -27,30 +28,6 @@ const invisibleMaterial = new THREE.MeshBasicMaterial({
     transparent: true,
     opacity:0,
 });
-
-const coneGeometry = new THREE.ConeGeometry(0.5, 2, 8);
-function ConeIndicator(props: {action: UnitAction, smoothing: boolean}) {
-    // TODO - this will be replaced with animations etc
-    let indicatorColor = 0xeeeeee;
-    if (props.action === 'Moving')
-        indicatorColor = 0x55ff55;
-    else if (props.action === 'Attacking')
-        indicatorColor = 0xff5555;
-    else if (props.action === 'Harvesting')
-        indicatorColor = 0x5555ff;
-    // indicate discrepancy between server and us
-    else if (props.smoothing)
-        indicatorColor = 0xffff55;
-
-    return (
-        <mesh
-            position={[0, 5, 0]}
-            rotation={[0, 0, -1.57]}
-            geometry={coneGeometry}
-            material={cache.getBasicMaterial(indicatorColor)}
-        />
-    );
-}
 
 type Unit3DProps = {
     unit: Unit,
